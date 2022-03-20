d66c623afed027526b3f7522a18ac106abc65be2
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-20 12:45:25 +0530

    Add files via upload

---
+++ FirstName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTest
+{
+    public class FirstName
+    {
+        public string GetFirstName(int empId)
+        {
+            string name;
+            if (empId == 1)
+            {
+                name = "pratik";
+            }
+           
+            else
+            {
+                name = "Not Found";
+            }
+            return name;
+
+            
+        }
+
+    }
+}
