6e9a2fb1b80adee783233f030424364edf06ca3c
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-20 12:50:58 +0530

    Add files via upload

---
+++ Email.cs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTest
+{
+    public class Email
+    {
+        public string GetEmail(int empId)
+        {
+            string email;
+            if (empId == 1)
+            {
+                email = "pratik.chitruk@gmail.com";
+            }
+
+            else
+            {
+                email = "Not Found";
+            }
+            return email;
+        }
+    }
+}
