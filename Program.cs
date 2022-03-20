1f060649e0f7c7c08b70e469400bd034f5dd0766
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-20 12:42:48 +0530

    program.cs push for MSTest

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MSTest 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ms test");
+
+            
+         
+            
+
+        }
+    }
+}
