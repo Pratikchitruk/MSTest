99a5f571147b1775128b2c181f68a083fc623868
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-20 12:48:21 +0530

    Add files via upload

---
+++ Password.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTest
+{
+    public class Password
+    {
+        public string GetPassword(int empId)
+        {
+            string password;
+            if (empId == 1)
+            {
+                password = "1234567";
+            }
+
+            else
+            {
+                password = "Not Found";
+            }
+            return password;
+        }
+    }
+}
