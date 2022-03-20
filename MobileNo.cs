89ca1a0fac929654ba7728cce7326380b60e83de
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-20 12:47:30 +0530

    Add files via upload

---
+++ MobileNo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTest
+{
+    public class MobileNo
+    {
+        public string GetPhoneNo(int empId)
+        {
+            string number;
+            if (empId == 1)
+            {
+                number = "8208508814";
+            }
+
+            else
+            {
+                number = "Not Found";
+            }
+            return number;
+        }
+    }
+}
